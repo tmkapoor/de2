051fdc370da8566540e08a81f29b6ac0d0f000df
Tanay Manohar Kapoor <tmkapoor@gmail.com>
2015-03-24 13:16:42 +0000

    added extra-info and timeline aminations

--- assets/js/mai.js
+++ assets/js/mai.js
@@ -4,10 +4,9 @@ var moreInfo = [
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
 		desc: "As seen from the Earth, a solar eclipse is a type of eclipse that occurs when the Moon passes between the Sun and Earth, and the Moon fully or partially blocks (\"occults\") the Sun.",
 		bits: {
-			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section3: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section4: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
+			Occurance: "Total solar eclipses are rare events. Although they occur somewhere on Earth every 18 months on average, it is estimated that they recur at any given place only once every 360 to 410 years, on average. The total eclipse lasts for only a maximum of a few minutes at any location, because the Moon's umbra moves eastward at over 1700 km/h.",
+			Frequency: "Between two and five solar eclipses occur every year, with at least one per eclipse season. Since the Gregorian calendar was instituted in 1582, years that have had five solar eclipses were 1693, 1758, 1805, 1823, 1870, and 1935. The next occurrence will be 2206. On average, there are about 240 solar eclipses each century.",
+			Types: "<ul><li><b>Total eclipse</b> occurs when the the Moon completely obscures the Sun.</li><li><b>Partial eclipse</b> occurs when the Sun and Moon are not exactly in line and the Moon only partially obscures the Sun.</li></ul>",
 		}
 	},
 	{
@@ -15,10 +14,10 @@ var moreInfo = [
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Jarlshof_20080821_-_aisled_roundhouse_and_broch.jpg/220px-Jarlshof_20080821_-_aisled_roundhouse_and_broch.jpg",
 		desc: "Shetland, also called the Shetland Islands, is a subarctic archipelago of Scotland that lies north-east of the island of Great Britain and forms part of the United Kingdom.",
 		bits: {
-			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section3: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section4: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
+			Etymology: "In AD 43 and 77 the Roman authors Pomponius Mela and Pliny the Elder referred to the seven islands they call Haemodae and Acmodae respectively, both of which are assumed to be Shetland.",
+			Geography: "Shetland is around 170 kilometres (110 mi) north of mainland Scotland, covers an area of 1,468 square kilometres (567 sq mi) and has a coastline 2,702 kilometres (1,679 mi) long. Only 16 of about 100 islands are inhabited. The main island of the group is known as Mainland, and of the next largest, Yell, Unst, and Fetlar lie to the north and Bressay and Whalsay lie to the east.",
+			Economy: "Today, the main revenue producers in Shetland are agriculture, aquaculture, fishing, renewable energy, the petroleum industry (crude oil and natural gas production), the creative industries and tourism.",
+			Climate: "Shetland has an oceanic sub-polar climate, with long but cool winters and short mild summers. The climate all year round is moderate due to the influence of the surrounding seas, with average peak temperatures of 7 °C (45 °F) in March and 18 °C (64 °F) in July and August.",
 		}
 	},
 	{
@@ -26,10 +25,9 @@ var moreInfo = [
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/V%C3%A5gakaillen_nattlys.JPG/240px-V%C3%A5gakaillen_nattlys.JPG",
 		desc: "The Norwegian Sea is a marginal sea in the North Atlantic Ocean, northwest of Norway. It is located between the North Sea (i.e. north of the United Kingdom) and the Greenland Sea and adjoins the North Atlantic Ocean to the west and the Barents Sea to the northeast.",
 		bits: {
-			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section3: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section4: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
+			"Formation": "The Norwegian Sea was formed about 250 million years ago, when the Eurasian plate of Norway and the North American Plate, including Greenland, started to move apart. The existing narrow shelf sea between Norway and Greenland began to widen and deepen. The coasts of the Norwegian Sea were shaped during the last Ice Age.",
+			"Flora and fauna": "The Norwegian Sea is a transition zone between boreal and Arctic conditions, and thus contains flora and fauna characteristic of both climatic regions. The Norwegian coastal waters are the most important spawning ground of the herring populations of the North Atlantic. Significant numbers of minke, humpback, sei, and orca whales are present in the Norwegian Sea, and white-beaked dolphins occur in the coastal waters.",
+			"Human activity": "Norway, Iceland, and Denmark/Faroe Islands share the territorial waters of the Norwegian Sea, with the largest part belonging to the first.",
 		}
 	},
 	{
@@ -37,10 +35,9 @@ var moreInfo = [
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/5/58/British_Isles_%28orthographic_projection%29.svg/220px-British_Isles_%28orthographic_projection%29.svg.png",
 		desc: "The British Isles are a group of islands off the north-western coast of continental Europe that consist of the islands of Great Britain, Ireland and over six thousand smaller isles. Two sovereign states are located on the islands: Ireland and the United Kingdom.",
 		bits: {
-			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section3: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section4: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
+			Etymology: "The earliest known references to the islands as a group appeared in the writings of sea-farers from the ancient Greek colony of Massalia..",
+			Geography: "The British Isles lie at the juncture of several regions with past episodes of tectonic mountain building. These orogenic belts form a complex geology which records a huge and varied span of Earth's history. The west coasts of Ireland and Scotland that directly face the Atlantic Ocean are generally characterised by long peninsulas, and headlands and bays; the internal and eastern coasts are smoother.",
+			Demographics: "The demographics of the British Isles today are characterised by a generally high density of population in England, which accounts for almost 80% of the total population of the islands. In elsewhere on Great Britain and on Ireland, high density of population is limited to areas around, or close to, a few large cities. The largest urban area by far is the Greater London Urban Area with 9 million inhabitants.",
 		}
 	},
 	{
@@ -48,22 +45,20 @@ var moreInfo = [
 		img: "http://newsimg.bbc.co.uk/media/images/45859000/jpg/_45859266_liz_bbc226b.jpg",
 		desc: "Elizabeth Bonnin (born 16 September 1976) is a French-born Irish television presenter who has worked on television in both Ireland and the United Kingdom. She works for the BBC, presenting Bang Goes the Theory and for ITV, presenting Countrywise.",
 		bits: {
-			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section3: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section4: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
+			Education: "Liz then decided that it was time to return to her science roots and in 2008 she completed a Masters in Wild Animal Biology with the Zoological Society of London, a course which involved setting up a research project and travelling to Nepal to study tigers.",
+			Career: "Bonnin began her career by joining an Irish girl pop group named Chill, who signed to Polydor but broke up before recording. In 2002 she became one of the presenters of the Channel 4 morning show RI:SE, specialising in reporting on entertainment-related stories. In the same year she became a regular presenter on Top of the Pops in the UK.",
+			"Latest projects": "‘Operation Snow Tiger’, a series on Amur tigers for BBC2, tracking the world’s largest big cat during the harsh winter of the Russian Far East and ‘Horizon: Tomorrow’s World’ also on BBC2, a special on the latest science and technology that’s set to transform our lives.",
 		}
 	},
 
 	{
 		title: "National Eclipse Weather Experiment",
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "On Friday 20 March 2015 there will be a rare near-total solar eclipse visible from the UK. The Department of Meteorology will be running a series of events and experiments, both at the University of Reading and elsewhere, during the eclipse.",
+		desc: "The Department of Meteorology will be running a series of events and experiments, both at the University of Reading and elsewhere, during the eclipse.",
 		bits: {
-			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section3: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
-			section4: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
+			"Where is it based ?": "This experiment is a join collaboration between the University of Reading and the Department of Meteorology.",
+			"Why do we need this experiment ?": "A solar eclipse briefly dims the radiation coming from the Sun, leading to cooling of the atmosphere with interesting effects on the weather.To study these effects, the Department of Meteorology at the University of Reading invites you to take part in NEWEx - National Eclipse Weather Experiment, a citizen science project to collect weather data during the solar eclipse for detailed analysis.",
+			"How to safely observe the solar eclipse ? ": "</br>An easy way to observe an eclipse is via “pinhole projection”. This is special arrangement to make sure that you avoid looking directly into the Sun, but instead you project its disc onto a screen or sheet of paper.",
 		}
 	},
 ];
@@ -109,7 +104,7 @@ $(document).ready(function($) {
 
 		    var content = "";
 
-		    content += "<div class='col-md-8 col-xs-12'> <h3>"+  contentItem["title"] +"</h3> <p>"+ contentItem['desc'] +"</p> </br> <a href='/stage3' class='btn btn-lg btn-primary' >View on timeline</a> </div>";
+		    content += "<div class='col-md-8 col-xs-12'> <h3>"+  contentItem["title"] +"</h3> <p>"+ contentItem['desc'] +"</p> </br> <a href='/stage3' class='btn btn-lg btn-primary' >Follow this topic</a></div>";
 
 		    content += "<div class='col-md-4 col-xs-12'> <img src='" + contentItem['img'] + "' /></div>";
 
@@ -219,24 +214,9 @@ $(document).ready(function($) {
 	}
 
 	if(page == "3"){
-		$(".timeline-scroll").height($(document).height() * 0.9);
-
-		$(".item-holder div").click(function(event) {
-			var extraInfo = $(this).find(".extra-info");
-
-			if(!(extraInfo.is(":visible"))){
-				$(".item-holder div").removeClass('highlight');
-
-				$(this).addClass('highlight');
-				var allExtraInfo = $(".extra-info");
-
-				allExtraInfo.slideUp('slow', function() {});
-				extraInfo.slideDown('slow', function() {});
-
-				$('html,body').animate({
-			          scrollTop: $(this).offset().top
-			        }, "slow");
-			}
+		$(".clickable").click(function(event) {
+			$(".removed").slideUp('slow');
+			$(this).find(".removed").slideDown('slow', function() {});
 		});
 	}
 
