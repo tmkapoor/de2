9beb8bb6c1437b6b06505dc2c920808f89a6a7f5
Tanay Manohar Kapoor <tmkapoor@gmail.com>
2015-03-24 10:07:18 +0000

    extra info

--- assets/js/mai.js
+++ assets/js/mai.js
@@ -2,7 +2,7 @@ var moreInfo = [
 	{
 		title: "Solar Eclipse",
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "Something...",
+		desc: "As seen from the Earth, a solar eclipse is a type of eclipse that occurs when the Moon passes between the Sun and Earth, and the Moon fully or partially blocks (\"occults\") the Sun.",
 		bits: {
 			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
 			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
@@ -12,8 +12,8 @@ var moreInfo = [
 	},
 	{
 		title: "Shetland Islands",
-		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "Something...",
+		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Jarlshof_20080821_-_aisled_roundhouse_and_broch.jpg/220px-Jarlshof_20080821_-_aisled_roundhouse_and_broch.jpg",
+		desc: "Shetland, also called the Shetland Islands, is a subarctic archipelago of Scotland that lies north-east of the island of Great Britain and forms part of the United Kingdom.",
 		bits: {
 			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
 			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
@@ -23,8 +23,8 @@ var moreInfo = [
 	},
 	{
 		title: "Norwegian Sea",
-		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "Something...",
+		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/V%C3%A5gakaillen_nattlys.JPG/240px-V%C3%A5gakaillen_nattlys.JPG",
+		desc: "The Norwegian Sea is a marginal sea in the North Atlantic Ocean, northwest of Norway. It is located between the North Sea (i.e. north of the United Kingdom) and the Greenland Sea and adjoins the North Atlantic Ocean to the west and the Barents Sea to the northeast.",
 		bits: {
 			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
 			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
@@ -34,8 +34,8 @@ var moreInfo = [
 	},
 	{
 		title: "British Isles",
-		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "Something...",
+		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/5/58/British_Isles_%28orthographic_projection%29.svg/220px-British_Isles_%28orthographic_projection%29.svg.png",
+		desc: "The British Isles are a group of islands off the north-western coast of continental Europe that consist of the islands of Great Britain, Ireland and over six thousand smaller isles. Two sovereign states are located on the islands: Ireland and the United Kingdom.",
 		bits: {
 			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
 			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
@@ -45,8 +45,8 @@ var moreInfo = [
 	},
 	{
 		title: "Liz Bonnin",
-		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "Something...",
+		img: "http://newsimg.bbc.co.uk/media/images/45859000/jpg/_45859266_liz_bbc226b.jpg",
+		desc: "Elizabeth Bonnin (born 16 September 1976) is a French-born Irish television presenter who has worked on television in both Ireland and the United Kingdom. She works for the BBC, presenting Bang Goes the Theory and for ITV, presenting Countrywise.",
 		bits: {
 			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
 			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
@@ -58,7 +58,7 @@ var moreInfo = [
 	{
 		title: "National Eclipse Weather Experiment",
 		img: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Solar_eclipse_1999_4_NR.jpg/609px-Solar_eclipse_1999_4_NR.jpg",
-		desc: "Something...",
+		desc: "On Friday 20 March 2015 there will be a rare near-total solar eclipse visible from the UK. The Department of Meteorology will be running a series of events and experiments, both at the University of Reading and elsewhere, during the eclipse.",
 		bits: {
 			section1: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
 			section2: "Lorem ipsum dolor sit amet, vix in omnes mollis appetere, elitr debitis mnesarchum ad pro. Cu duo modo debet, sed alterum adversarium ne, euismod tibique intellegebat ne per.",
